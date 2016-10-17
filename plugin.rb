669584befe616e83213c42a9e4cc4f48a167a813
Sébastien Bourdu <git@ekkans.com>
2016-10-17 19:45:59 +0200

    Added enabled_site_setting

--- plugin.rb
+++ plugin.rb
@@ -2,5 +2,8 @@
 # about: La ruche qui dit oui
 # version: 0.0.1
 # authors: Sébastien Bourdu
+# url: https://github.com/ekkans/lrqdo-plugin-discourse
+
+enabled_site_setting :lrqdo_enabled
 
 register_asset 'stylesheets/theme-1476703501.css'
