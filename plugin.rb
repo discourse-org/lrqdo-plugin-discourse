a3f50f95fea8e57ea4edd632c5920287503e5a61
Sébastien Bourdu <git@ekkans.com>
2016-10-17 14:28:15 +0200

    Moved fonts on S3

--- plugin.rb
+++ plugin.rb
@@ -3,24 +3,4 @@
 # version: 0.0.1
 # authors: Sébastien Bourdu
 
-register_asset 'fonts/leaguegothic-condensed-italic-webfont.eot'
-register_asset 'fonts/leaguegothic-condensed-italic-webfont.svg'
-register_asset 'fonts/leaguegothic-condensed-italic-webfont.ttf'
-register_asset 'fonts/leaguegothic-condensed-italic-webfont.woff'
-
-register_asset 'fonts/leaguegothic-condensed-regular-webfont.eot'
-register_asset 'fonts/leaguegothic-condensed-regular-webfont.svg'
-register_asset 'fonts/leaguegothic-condensed-regular-webfont.ttf'
-register_asset 'fonts/leaguegothic-condensed-regular-webfont.woff'
-
-register_asset 'fonts/leaguegothic-italic-webfont.eot'
-register_asset 'fonts/leaguegothic-italic-webfont.svg'
-register_asset 'fonts/leaguegothic-italic-webfont.ttf'
-register_asset 'fonts/leaguegothic-italic-webfont.woff'
-
-register_asset 'fonts/leaguegothic-regular-webfont.eot'
-register_asset 'fonts/leaguegothic-regular-webfont.svg'
-register_asset 'fonts/leaguegothic-regular-webfont.ttf'
-register_asset 'fonts/leaguegothic-regular-webfont.woff'
-
-register_asset 'stylesheets/theme-80bdbdea.css'
+register_asset 'stylesheets/theme-1476703501.css'
