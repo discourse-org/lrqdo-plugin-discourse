161941911fc966d8e7c51d447f04acd93d40029a
Sébastien Bourdu <git@ekkans.com>
2016-10-17 11:13:01 +0200

    Removed artifacts

--- plugin.rb
+++ plugin.rb
@@ -2,7 +2,3 @@
 # about: La ruche qui dit oui
 # version: 0.0.1
 # authors: Sébastien Bourdu
-
-enabled_site_setting :override_extra_info
-
-register_asset 'stylesheets/header-search.scss', :desktop
