ab41367c06da5d1ab253b71da7e7a0d73f3f66ce
Sébastien Bourdu <git@ekkans.com>
2016-10-17 12:59:20 +0200

    Added lrqdo theme to plugin

--- plugin.rb
+++ plugin.rb
@@ -2,3 +2,25 @@
 # about: La ruche qui dit oui
 # version: 0.0.1
 # authors: Sébastien Bourdu
+
+register_asset 'fonts/leaguegothic-condensed-italic-webfont.eot'
+register_asset 'fonts/leaguegothic-condensed-italic-webfont.svg'
+register_asset 'fonts/leaguegothic-condensed-italic-webfont.ttf'
+register_asset 'fonts/leaguegothic-condensed-italic-webfont.woff'
+
+register_asset 'fonts/leaguegothic-condensed-regular-webfont.eot'
+register_asset 'fonts/leaguegothic-condensed-regular-webfont.svg'
+register_asset 'fonts/leaguegothic-condensed-regular-webfont.ttf'
+register_asset 'fonts/leaguegothic-condensed-regular-webfont.woff'
+
+register_asset 'fonts/leaguegothic-italic-webfont.eot'
+register_asset 'fonts/leaguegothic-italic-webfont.svg'
+register_asset 'fonts/leaguegothic-italic-webfont.ttf'
+register_asset 'fonts/leaguegothic-italic-webfont.woff'
+
+register_asset 'fonts/leaguegothic-regular-webfont.eot'
+register_asset 'fonts/leaguegothic-regular-webfont.svg'
+register_asset 'fonts/leaguegothic-regular-webfont.ttf'
+register_asset 'fonts/leaguegothic-regular-webfont.woff'
+
+register_asset 'stylesheets/theme-80bdbdea.css'
